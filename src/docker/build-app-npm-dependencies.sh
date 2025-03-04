3608e87728cc95925e52e44162e5597fb87c3ad8
Arian Jahiri <69126638+arianjahiri@users.noreply.github.com>
2025-03-04 16:41:53 +1100

    Update build-app-npm-dependencies.sh

--- src/docker/build-app-npm-dependencies.sh
+++ src/docker/build-app-npm-dependencies.sh
@@ -6,4 +6,4 @@ printf "\n[-] Installing app NPM dependencies...\n\n"
 
 cd $APP_SOURCE_FOLDER
 
-meteor npm ci || meteor npm install # The latter is for older versions of Meteor that ship with npm < 5.7.0
+meteor npm ci --legacy-peer-deps || meteor npm install --legacy-peer-deps # The latter is for older versions of Meteor that ship with npm < 5.7.0
